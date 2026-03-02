1804ca987fba8a7ea005d33a7c55de31fe0548e8
Matt Bryant <brybrant@gmail.com>
2026-03-02 06:57:43 +0000

    Simplify eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,48 +1,56 @@
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-import { eslintConfig } from '@brybrant/configs';
+import { eslintConfig, prettierConfig } from '@brybrant/configs';
+
+const prettierRules = Object.assign({}, eslintPluginPrettier.rules, {
+  'prettier/prettier': ['error', prettierConfig],
+});
 
 export default eslintConfig(
-  {
-    files: ['**/*.ts', '**/*.js'],
-    ignores: ['**/dist/**'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-    },
-  },
-  {
-    files: ['*.js'],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-    },
-  },
-  ...tseslint.configs.strictTypeChecked.map((config) => ({
-    ...config,
-    files: ['**/*.ts'],
-  })),
   {
     files: ['**/*.ts'],
     plugins: {
       '@typescript-eslint': tseslint.plugin,
+      prettier: eslintPluginPrettier.plugins.prettier,
     },
     languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
       parser: tseslint.parser,
       parserOptions: {
         project: true,
       },
     },
-    rules: {
-      '@typescript-eslint/restrict-template-expressions': [
-        1,
-        {
-          allowNumber: true,
-        },
-      ],
+    rules: Object.assign(
+      {},
+      tseslint.configs.strictTypeChecked.reduce(
+        (rules, config) => Object.assign(rules, config.rules ?? {}),
+        {},
+      ),
+      {
+        '@typescript-eslint/restrict-template-expressions': [
+          1,
+          {
+            allowNumber: true,
+          },
+        ],
+      },
+      prettierRules,
+    ),
+  },
+  {
+    files: ['**/*.js'],
+    plugins: {
+      prettier: eslintPluginPrettier.plugins.prettier,
     },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: prettierRules,
   },
 );
